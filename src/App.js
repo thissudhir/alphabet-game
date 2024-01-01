65c04c6311f7455bd5e1e73497a32d72357667a8
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2024-01-01 23:39:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const alphabetList = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+const App = () => {
+  const [selectedAlphabet, setSelectedAlphabet] = useState(null);
+
+  const handleAlphabetClick = (alphabet) => {
+    setSelectedAlphabet(alphabet);
+    const audio = new Audio(
+      `${process.env.PUBLIC_URL}/${alphabet.toLowerCase()}.m4a`
+    );
+    audio.play();
+    // };
+  };
+
+  return (
+    <div className="App">
+      <h1>Alphabet Game</h1>
+      <h2>Select an Alphabet</h2>
+      <div className="alphabet-container">
+        {alphabetList.map((alphabet) => (
+          <div
+            key={alphabet}
+            className={`alphabet ${
+              selectedAlphabet === alphabet ? "selected" : ""
+            }`}
+            onClick={() => handleAlphabetClick(alphabet)}
+          >
+            {alphabet}
+          </div>
+        ))}
+      </div>
+      {selectedAlphabet && (
+        <div className="gif-container">
+          <img
+            src={`${process.env.PUBLIC_URL}/${selectedAlphabet}.gif`}
+            alt={`GIF for ${selectedAlphabet}`}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
